eb3689fd860595a45b48d5e0d109ecdc5bb5de50
Mark Visschers <visschers.m@gmail.com>
2021-06-18 08:59:06 +0200

    Added src folder for none-test projects

---
+++ src/TestBase.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace MatrTech.Utilities.Test
+{
+    public abstract class TestBase
+    {
+        protected readonly GenericFactory factory = new GenericFactory();
+    }
+}
