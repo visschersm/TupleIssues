3cf9d808379c331a60a74dbd27c9b3523c576078
Mark Visschers <visschers.m@gmail.com>
2021-08-08 11:31:07 +0200

    moved to separate attribute folders + adding more tests

---
+++ src/Utilities.Test/Attributes/JsonTestMethodAttribute_462.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Text;
+using Newtonsoft.Json;
+using System.Linq;
+using System.Collections;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Matr.Utilities.Test.Attributes
+{
+    [AttributeUsage(System.AttributeTargets.Method)]
+    public class JsonTestMethodAttribute : TestMethodAttribute
+    {
+        private readonly string filepath;
+        private readonly Type dataType;
+        public JsonTestMethodAttribute(string filepath, Type dataType)
+        {
+            this.filepath = filepath;
+            this.dataType = dataType;
+        }
+
+        public override TestResult[] Execute(ITestMethod testMethod)
+        {
+            using (StreamReader r = new StreamReader(filepath))
+            {
+                string json = r.ReadToEnd();
+
+                var dataArray = JsonConvert.DeserializeObject(json, dataType.MakeArrayType());
+                return (dataArray as IEnumerable).Cast<object>()
+                    .Select(x => testMethod.Invoke(new object[] { x }))
+                    .ToArray();
+            }
+        }
+    }
+}
