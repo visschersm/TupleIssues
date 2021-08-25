7a490cad32c86735b675cb853fe994a17c1b36da
Mark Visschers <visschers.m@gmail.com>
2021-08-25 14:15:17 +0200

    resolved feedback

--- tests/Utilities.Test.NUnitTests/NUnitTestExamples.cs
+++ tests/Utilities.Test.NUnitTests/NUnitTestExamples.cs
@@ -9,21 +9,16 @@ namespace Matr.Utilities.Test.UnitTests
 {
     public class NUnitTestExamples : TestBase
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
 
         [Test]
         public void SimpleExample_WithDependency_ResolvesMockedDependency()
         {
-            var random = new Random();
-            var returnValue = random.Next(1, int.MaxValue);
+            var returnValue = 1;
 
             var mockedDependency = new Mock<ITestDependency>();
             mockedDependency.Setup(x => x.Add(It.IsAny<int>(), It.IsAny<int>()))
                 .Returns(returnValue);
-            
+
             factory.RegisterOrReplaceService(mockedDependency.Object);
             var testClass = factory.Create<TestClass>();
 
@@ -41,10 +36,10 @@ namespace Matr.Utilities.Test.UnitTests
                 this.dependency = testDependency;
             }
 
-           public int DoAddition(int x, int y)
-           {
+            public int DoAddition(int x, int y)
+            {
                 return dependency.Add(x, y);
-           }
+            }
         }
 
         public interface ITestDependency
